5606b321356286dbdd84fa01eaa2a7ae7cad5555
Bhavya33060 <yarlagaddabhavya27@gmail.com>
2025-09-06 00:35:10 +0530

    Update CruddemoApplication.java

--- crud_back/src/main/java/com/klu/CruddemoApplication.java
+++ crud_back/src/main/java/com/klu/CruddemoApplication.java
@@ -1,13 +1,18 @@
-package com.klu;
+package com.klu.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class CruddemoApplication {
+@RestController
+public class HomeController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot app is running!";
+    }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
 }
